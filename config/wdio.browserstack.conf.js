4ad772ab377fbca6658b7b4322a3e7a4c60c4416
Kervinn Aguilera <kervinnaguilera@gmail.com>
2025-07-06 20:24:13 -0300

    BrowserStack Integration

---
+++ config/wdio.browserstack.conf.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { config } = require('./wdio/wdio.conf');
+
+exports.config = {
+    ...config,
+    
+    // BrowserStack specific configuration
+    user: 'kervinnaguilera_7bg03n',
+    key: 'aczkaqoJEpWZpaGVYeMs',
+    
+    services: [
+        ['browserstack', {
+            browserstackLocal: false
+        }]
+    ],
+    
+    capabilities: [{
+        platformName: 'android',
+        'appium:deviceName': 'Samsung Galaxy S23',
+        'appium:osVersion': '13.0',
+        'appium:app': 'bs://07d8d0a0a625abfa6ef3dd5404181a5963271d27',
+        'bstack:options': {
+            projectName: 'Cross Mobile Test Automation',
+            buildName: 'Android Tests',
+            sessionName: 'Login Tests',
+            debug: true,
+            networkLogs: true,
+            deviceLogs: true
+        }
+    }],
+    
+    // BrowserStack specific specs
+    specs: [
+        path.resolve(__dirname, '../tests/android/specs/login/login.test.js')
+    ],
+    
+    // BrowserStack specific hooks
+    before: function (capabilities, specs) {
+        console.log('Starting BrowserStack test session...');
+    },
+    
+    after: function (result, capabilities, specs) {
+        console.log('BrowserStack test session completed');
+    }
+}; 
